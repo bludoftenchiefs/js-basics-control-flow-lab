504059de2edc8a79900d277af56fd800c96107cb
rilith <bludoftenchiefs@protonmail.com>
2021-04-29 22:10:25 -0500

    commit tree a3511cacfc0948fea6e60d4b7004b151e7f52bd8

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue >= 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+  console.log(scuberGreetingForFeet(2501));
+} 
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  return (someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
+ternaryCheckCity('NYC');
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+function switchOnCharmFromTip(order){
+  let response;
+  switch (order) {
+    case 'generous':
+       response ='Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    case 'thanks for everything':
+      response = 'Bye.'
+      break;
+  }
+  return response;
 }
